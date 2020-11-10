df6ef75ca18c7c081c06ab18a2ae04fc21aa1213
AbdulMunim343 <munimwork90@gmail.com>
2020-11-11 01:26:16 +0500

    dropdowns

--- src/common/topbar/Topbar.js
+++ src/common/topbar/Topbar.js
@@ -4,12 +4,46 @@ import '../topbar/Topbar.scss';
 //==Import Icons==//
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import PublicIcon from '@material-ui/icons/Public';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
+import MessageOutlinedIcon from '@material-ui/icons/MessageOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 
 //==import images for topbar==//
 import Search from '../../assets/img/dashboard/search.svg';
 import Admin from '../../assets/img/dashboard/admin.jpg';
 import Envelop from '../../assets/img/dashboard/envelop.svg';
 import Notification from '../../assets/img/dashboard/notification.svg';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+
+
+const Wrapper = () =>{
+   return(
+      <div className="contant_area">
+      <div className="messege_wrapper">
+         
+         <div className="user_img">
+            <img src={Admin}/>
+         </div>
+
+         <div class="text_wrapper">
+               <div className="nt_container">
+                  <span className="item_name">Maria Zaman</span>
+                  <span className="item_time">18:30</span>
+               </div>
+         <p>
+            What is the reason of buy this item. Is it usefull for me.....
+         </p>
+         
+         </div>
+         
+      </div>
+      
+      </div>
+   )
+}
+
+
 
 const Topbar = () =>{
     return(
@@ -27,6 +61,45 @@ const Topbar = () =>{
          <p className="pash">
             Admin
          </p>
+      
+      {/* Admin Drop Down start*/}
+
+         <div className="admin_drop_down">
+            <div className="admin_name">
+               Abdul Munim
+            </div>
+
+            <ul className="list">
+               <li>
+                     <PersonOutlineOutlinedIcon className="list_icn"/>
+                     My Profile
+               </li>
+
+               <li>
+                     <ListAltOutlinedIcon className="list_icn"/>
+                     Task
+               </li>
+
+               <li>
+                     <MessageOutlinedIcon className="list_icn"/>
+                     Messege
+               </li>
+
+               <li>
+                     <SettingsOutlinedIcon className="list_icn"/>
+                     Account Setting
+               </li>
+
+               <li>
+                     <ExitToAppOutlinedIcon  className="list_icn"/>
+                     Log Out
+               </li>
+            </ul>
+
+         </div>
+      
+      {/* Admin Drop Down End */}
+      
       </div>
       <div className="user_img">
          <img src={Admin}/>
@@ -34,13 +107,30 @@ const Topbar = () =>{
       <div className="notifications">
          
          <div className="envelop">
-            <img src={Envelop}/>
-            <span >8</span>
+            <img src={Envelop} className="enve"/>
+            <span className="Number_genrater">8</span>
+         
+         {/* Messege drop Down Start */}
+         
+         <div className="messege_drop_down">
+            <div className="messege_status">
+               05 Messege 
+            </div>
+
+          
+            <Wrapper/>
+            <Wrapper/>
+            <Wrapper/>
+            <Wrapper/>
+
          </div>
+         {/* Messege drop Down end */}
+         
+      </div>
          
          <div className="bell">
             <img src={Notification} />
-            <span >8</span>
+            <span className="Number_genrater">8</span>
          </div>
       
       </div>
